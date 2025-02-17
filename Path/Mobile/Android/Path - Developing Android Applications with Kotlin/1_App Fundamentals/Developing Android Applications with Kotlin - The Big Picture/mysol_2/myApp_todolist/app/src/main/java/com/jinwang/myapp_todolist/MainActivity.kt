451f81eaa54259f691e2152532a8e4e8fbe3f95e
Jin Wang <specter01wj@gmail.com>
2025-02-16 20:39:18 -0500

    v4

--- Path/Mobile/Android/Path - Developing Android Applications with Kotlin/1_App Fundamentals/Developing Android Applications with Kotlin - The Big Picture/mysol_2/myApp_todolist/app/src/main/java/com/jinwang/myapp_todolist/MainActivity.kt
+++ Path/Mobile/Android/Path - Developing Android Applications with Kotlin/1_App Fundamentals/Developing Android Applications with Kotlin - The Big Picture/mysol_2/myApp_todolist/app/src/main/java/com/jinwang/myapp_todolist/MainActivity.kt
@@ -12,20 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.jinwang.myapp_todolist.ui.theme.MyApp_todolistTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApp_todolistTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            TodoApp()
         }
     }
 }
@@ -45,3 +39,10 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+@Composable
+fun TodoApp(viewModel: TaskViewModel = viewModel()) {
+    // UI will be implemented in the next step
+}
+
+

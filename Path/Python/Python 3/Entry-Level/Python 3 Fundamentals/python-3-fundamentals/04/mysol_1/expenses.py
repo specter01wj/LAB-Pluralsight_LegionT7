783f968aa7f3d5104ffb4b15be0f46605fe5ec32
Jin Wang <specter01wj@gmail.com>
2025-03-02 17:57:46 -0500

    v2

--- Path/Python/Python 3/Entry-Level/Python 3 Fundamentals/python-3-fundamentals/04/mysol_1/expenses.py
+++ Path/Python/Python 3/Entry-Level/Python 3 Fundamentals/python-3-fundamentals/04/mysol_1/expenses.py
@@ -1,5 +1,12 @@
 expenses = [10.50, 8, 5, 15, 20, 5, 3]
 
+sum1 = 0
+
+for expense in expenses:
+    sum1 += expense
+
+print('sum = ', sum1)
+
 total = sum(expenses)
 
 print('You spent $', total, sep = '')
